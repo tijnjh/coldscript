f2da09e8ae1c73c763e1eec6ddeb39ce4b32b451
tijn <hoi@tijn.dev>
2026-06-26 12:31:30 +0200

    feat: add fromEntries and assign functions to objects module

--- src/objects.ts
+++ src/objects.ts
@@ -1,5 +1,11 @@
 import type { Call, Objects } from "hotscript";
 
+export function fromEntries<
+  O extends Iterable<readonly [PropertyKey, unknown]>,
+>(o: O) {
+  return Object.fromEntries(o) as Call<Objects.FromEntries, O>;
+}
+
 export function entries<O extends object>(o: O) {
   return Object.entries(o) as Call<Objects.Entries, O>;
 }
@@ -10,4 +16,8 @@ export function keys<O extends object>(o: O) {
 
 export function values<O extends object>(o: O) {
   return Object.values(o) as Call<Objects.Values, O>;
+}
+
+export function assign<O extends object, TO2 extends object>(o: O, o2: TO2) {
+  return Object.assign(o, o2) as unknown as Call<Objects.Assign<TO2>, O>;
 }
